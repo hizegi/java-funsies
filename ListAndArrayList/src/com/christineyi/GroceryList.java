75bc675bcd276222efbe0c74098c5ff128c9b7c3
hizegi <ohchristineyi@gmail.com>
2017-05-11 10:48:22 -0400

    grocery list array lists

---
+++ ListAndArrayList/src/com/christineyi/GroceryList.java
@@ -0,0 +1,44 @@
+package com.christineyi;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Christine on 5/11/17.
+ */
+public class GroceryList {
+//    private int[] myNumbers = new int[50];
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item){
+        groceryList.add(item);
+    }
+
+    public void printGroceryList(){
+        System.out.println("You have " + groceryList.size() + " items." );
+        for (int i = 0; i < groceryList.size(); i++){
+            System.out.println((i + 1) + " . " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem){
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified.");
+    }
+
+    public void removeGroceryItem(int position){
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+        //Milk
+        //Cheese
+        //Bread
+    }
+
+    public String findItem(String searchItem){
+//        boolean exists = groceryList.contains(searchItem);
+          int position = groceryList.indexOf(searchItem);
+          if(position >= 0){
+              return groceryList.get(position);
+          }
+          return null;
+    }
+}
