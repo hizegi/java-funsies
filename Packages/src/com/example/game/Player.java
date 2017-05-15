9d9ae1de94dac88feca5d56b252ff3bcc8866773
hizegi <ohchristineyi@gmail.com>
2017-05-15 17:42:00 -0400

    packages

---
+++ Packages/src/com/example/game/Player.java
@@ -0,0 +1,7 @@
+package com.example.game;
+
+/**
+ * Created by Christine on 5/15/17.
+ */
+public class Player {
+}
---
+++ Packages/src/com/example/mypackage/Main.java
@@ -0,0 +1,7 @@
+package com.example.mypackage;
+
+/**
+ * Created by Christine on 5/15/17.
+ */
+public class Main {
+}
